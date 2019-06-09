b0c1a80673db17b8f408763fb54c57aef19faa48
lovelope <lovelope@qq.com>
2019-06-09 12:58:34 +0800

    chore(deps): upgrade deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -12,18 +16,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
+        use: ['vue-style-loader', 'css-loader']
       },
       {
         test: /\.less$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'less-loader'
-        ],
+        use: ['vue-style-loader', 'css-loader', 'less-loader']
       },
       {
         test: /\.vue$/,
@@ -33,11 +30,7 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'less': [
-              'vue-style-loader',
-              'css-loader',
-              'less-loader'
-            ],
+            less: ['vue-style-loader', 'css-loader', 'less-loader']
           }
           // other vue-loader options go here
         }
@@ -58,8 +51,8 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      'src': path.resolve(__dirname, './src')
+      vue$: 'vue/dist/vue.esm.js',
+      src: path.resolve(__dirname, './src')
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
@@ -71,11 +64,13 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
-}
+  devtool: '#eval-source-map',
+  plugins: [new VueLoaderPlugin()]
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.mode = 'production';
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -83,14 +78,66 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
-  ])
+  ]);
+
+  module.exports.optimization = {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        terserOptions: {
+          ecma: undefined,
+          warnings: false,
+          parse: {},
+          compress: {
+            drop_debugger: true, // 删除 debugger
+            drop_console: true // 删除 console
+          },
+          mangle: true,
+          module: false,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          safari10: true
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({
+        assetNameRegExp: /\.css\.*(?!.*map)/g, // 注意不要写成 /\.css$/g
+        // eslint-disable-next-line global-require
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: {
+          // 使用安全模式，避免 cssnano 重新计算 z-index
+          safe: true,
+
+          // 默认不移除许可证注释，这里移除所有
+          discardComments: { removeAll: true },
+
+          // cssnano 集成了autoprefixer的功能
+          // 会使用到autoprefixer进行无关前缀的清理
+          // 关闭autoprefixer功能
+          // 使用postcss的autoprefixer功能
+          autoprefixer: false
+        },
+        canPrint: true
+      })
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          enforce: true
+        }
+      }
+    }
+  };
 }
